43cf0b101f8cc04644827eeda11bdf42abc4fecf
Ehsan <100062123+enikfar@users.noreply.github.com>
2022-11-09 10:46:15 -0500

    commmit

--- sort.c
+++ sort.c
@@ -13,15 +13,15 @@
 //     }
 // }
 
-int strcicmp(char const *a, char const *b)
-{
-    for (;; a++, b++)
-    {
-        int d = tolower((unsigned char)*a) - tolower((unsigned char)*b);
-        if (d != 0 || !*a)
-            return d;
-    }
-}
+// int strcicmp(void *a, void *b)
+// {
+//     for (;; a++, b++)
+//     {
+//         int d = tolower((unsigned char)*a) - tolower((unsigned char)*b);
+//         if (d != 0 || !*a)
+//             return d;
+//     }
+// }
 
 int cmp(void *p, void *q)
 {
@@ -32,13 +32,21 @@ int cmp(void *p, void *q)
 
     return strcmp(pp, qq);
 }
+int rev_cmp(void *p, void *q)
+{
+    char *pp = *(char **)p;
+    char *qq = *(char **)q;
+    // printf("%s\n", pp);
+    // printf("%s\n", qq);
 
+    return -strcmp(pp, qq);
+}
 int main(int arg_count, char **arg_values)
 {
-    for (int i = 0; i < arg_count; i++)
-    {
-        printf("%d: %s\n", i, arg_values[i]);
-    }
+    // for (int i = 0; i < arg_count; i++)
+    // {
+    //     printf("%d: %s\n", i, arg_values[i]);
+    // }
     char ch;
     char **strings;
     strings = malloc(1024 * 1024);
@@ -76,33 +84,36 @@ int main(int arg_count, char **arg_values)
         }
     }
 
-    qsort(strings, (row - 1), sizeof(char *), cmp);
+    // qsort(strings, (row - 1), sizeof(char *), cmp);
+
+    // free_array(strings);
+    return 0;
+
+    if (arg_count > 1)
+    {
+        for (int i = 1; i < arg_count; i++)
+        {
+            // if (strcmp(arg_values[i], "-f") == 0)
+            // {
+
+            //     qsort(strings, (row - 1), sizeof(char *), strcicmp);
+            // }
+            if (strcmp(arg_values[i], "-r") == 0)
+            {
+                qsort(strings, (row - 1), sizeof(char *), rev_cmp);
+                printf("here");
+            }
+            // if (strcmp(arg_values[i], "-n") == 0)
+            // {
+            //     qsort(strings, (row - 1), 1024, numeric_compar);
+            // }
+        }
+    }
+
     int i = 0;
 
     for (i; i < row - 1; i++)
     {
         printf("\n %s", strings[i]);
     }
-    // free_array(strings);
-    return 0;
-
-    // if (arg_count > 1)
-    // {
-    //     for (int i = 1; i < arg_count; i++)
-    //     {
-    //         if (strcmp(arg_values[i], "-f") == 0)
-    //         {
-
-    //             qsort(strings, (row - 1), 1024, strcicmp);
-    //         }
-    //         if (strcmp(arg_values[i], "-r") == 0)
-    //         {
-    //             qsort(strings, (row - 1), 1024, strcicmp);
-    //         }
-    //         if (strcmp(arg_values[i], "-n") == 0)
-    //         {
-    //             qsort(strings, (row - 1), 1024, numeric_compar);
-    //         }
-    //     }
-    // }
 }
