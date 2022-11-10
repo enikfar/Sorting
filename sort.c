32ff5eb45c35299c7d31f4cb91b921d843138412
Ehsan <100062123+enikfar@users.noreply.github.com>
2022-11-09 21:16:13 -0500

    final editing

--- sort.c
+++ sort.c
@@ -3,45 +3,91 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-void free_array(char **array, int rows)
+void free_array(char **array, int rows) // this free the array after sorting
 {
-    int row, col;
 
-    for (row = 0; row < rows; row++)
+    for (int row = 0; row < rows; row++)
+
     {
         free(array[row]);
     }
 }
 void print_array(char **array, int length)
 {
-    int row, col;
 
-    for (row = 0; row < length; row++)
+    for (int row = 0; row < length; row++)
     {
         printf("%s \n", array[row]);
     }
 }
-int strcicmp(void *p, void *q)
+int strcicmp(const void *p, const void *q) // this function compare the strigns- case insensitive
 {
     char *pp = *(char **)p;
     char *qq = *(char **)q;
-    for (; *pp; ++pp)
-        *pp = tolower(*pp);
-    for (; *qq; ++qq)
-        *qq = tolower(*qq);
-    return strcmp(pp, qq);
+
+    if (((pp[0] >= 'A') && (pp[0] <= 'Z')) && ((qq[0] >= 'A') && (qq[0] <= 'Z'))) // if both of them start with upper case, I just subtract them
+
+    {
+
+        if ((pp[0] - qq[0]) > 0)
+        {
+            return 1;
+        }
+        if ((pp[0] - qq[0]) < 0)
+        {
+            return (-1);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    if (((pp[0] >= 'A') && (pp[0] <= 'Z'))) // if one of them start with upper case
+    {
+
+        if (((pp[0]) - (qq[0] - 32)) > 0)
+        {
+            return 1;
+        }
+        if (((pp[0]) - (qq[0] - 32)) < 0)
+        {
+            return (-1);
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    if (((qq[0] >= 'A') && (qq[0] <= 'Z')))
+    {
+
+        if (((pp[0] - 32) - (qq[0])) > 0)
+        {
+            return 1;
+        }
+        if (((pp[0] - 32) - (qq[0])) < 0)
+        {
+            return (-1);
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        return strcmp(pp, qq); // if none of them are in alphabets, then we do the usual papers
+    }
 }
 
-int cmp(void *p, void *q)
+int cmp(const void *p, const void *q) // just normal comarsion of the strings
 {
     char *pp = *(char **)p;
     char *qq = *(char **)q;
-    // printf("%s\n", pp);
-    // printf("%s\n", qq);
 
     return strcmp(pp, qq);
 }
-int rev_cmp(void *p, void *q)
+int rev_cmp(const void *p, const void *q) // reverseing order
 {
     char *pp = *(char **)p;
     char *qq = *(char **)q;
@@ -54,12 +100,12 @@ long mystrtol(char *start, char **rest)
     p = start;
     int is_neg = 0;
     long acc = 0;
-    while (isspace(*p))
+    while (isspace(*p)) // remove all the leading spaces
     {
         p += 1;
         acc++;
     }
-    if (*p == '-')
+    if (*p == '-') // determine the sign
     {
         is_neg = 1;
         p += 1;
@@ -77,25 +123,25 @@ long mystrtol(char *start, char **rest)
         if ((*p >= '0') && (*p <= '9'))
         {
             digit = (*p - '0');
-	    p++;
+            p++;
             acc++;
         }
         else
         {
-            break;
+            break; // I finish the while when ther is no number
         }
         result = result * 10 + digit;
     }
     *rest = p;
-    printf("%d\n",result);
-    if(is_neg){
-    return ( (-1) * result);
-    }
 
+    if (is_neg)
+    {
+        return ((-1) * result);
+    }
 
     return result;
 }
-int num_cmp(void *p, void *q)
+int num_cmp(const void *p, const void *q)
 {
     char *rest = NULL;
     char *rest2 = NULL;
@@ -113,10 +159,7 @@ int num_cmp(void *p, void *q)
 
 int main(int arg_count, char **arg_values)
 {
-    // for (int i = 0; i < arg_count; i++)
-    // {
-    //     printf("%d: %s\n", i, arg_values[i]);
-    // }
+
     char ch = getchar();
     char **strings;
     strings = malloc(1024 * 1024);
@@ -124,7 +167,7 @@ int main(int arg_count, char **arg_values)
     while ((ch != EOF))
     {
         int col = 0;
-        // strings[row] = malloc(1024);
+
         char *word;
         word = malloc(1024 * sizeof(char) + 1);
 
@@ -134,31 +177,19 @@ int main(int arg_count, char **arg_values)
             {
                 break;
             }
-            // printf("%d \n", col);
+
             word[col] = ch;
-	    ch = getchar();
+            ch = getchar();
             col++;
         }
-        // printf("%d\n", row);
 
         word[col] = '\0';
-	ch = getchar();
+        ch = getchar();
 
         strings[row] = word;
-        // printf("\n %s", strings[row - 2]);
-        // printf(" \n here");
-        // printf("\n %s", word);
+
         row++;
-        if (ch == EOF)
-        {
-            strings[row] = malloc(1024);
-            strings[row] = '\0';
-        }
     }
-
-    // qsort(strings, (row - 1), sizeof(char *), cmp);
-
-    // free_array(strings);
 
     if (arg_count > 1)
     {
@@ -166,31 +197,33 @@ int main(int arg_count, char **arg_values)
         {
             if (strcmp(arg_values[i], "-f") == 0)
             {
-                ;
-                qsort(strings, (row - 1), sizeof(char *), strcicmp);
-                print_array(strings, row);
+
+                qsort(strings, (row), sizeof(char *), strcicmp);
             }
             else if (strcmp(arg_values[i], "-r") == 0)
             {
-                qsort(strings, (row - 1), sizeof(char *), rev_cmp);
-                print_array(strings, row);
+                qsort(strings, (row), sizeof(char *), rev_cmp);
             }
             else if (strcmp(arg_values[i], "-h") == 0)
             {
-                qsort(strings, (row - 1), sizeof(char *), cmp);
-                print_array(strings, row);
+                qsort(strings, (row), sizeof(char *), cmp);
             }
             else if (strcmp(arg_values[i], "-n") == 0)
             {
-                qsort(strings, row , sizeof(char *), num_cmp);
-                print_array(strings, row);
+                qsort(strings, row, sizeof(char *), num_cmp);
             }
             else
             {
                 printf("wrong flag");
+                exit(0);
             }
         }
     }
+    else
+    {
+        qsort(strings, (row - 1), sizeof(char *), cmp);
+    }
+    print_array(strings, row);
     free_array(strings, row);
     return 0;
 }
